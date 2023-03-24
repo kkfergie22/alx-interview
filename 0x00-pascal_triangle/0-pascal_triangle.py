7bc05183a8e15c94fef0dd0063d5cc440fb5f248
Christian Koranteng <korantengchristian@gmail.com>
2023-03-24 05:02:00 +0000

    Upload files

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+def pascal_triangle(n):
+    if (n == 0):
+        return []
+    if (n == 1):
+        return [[1]]
+    result = []
+    for row in range(1, n+1):
+        arr = []
+        for col in range(0, row):
+            if (col == 0 or col == row - 1):
+                arr.append(1)
+            else:
+                arr.append(result[row-2][col-1] + result[row-2][col])
+        result.append(arr)
+    return result
