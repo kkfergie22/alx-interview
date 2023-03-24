2313b22be9a2a74e5a623a7cc76eda8c98828fb0
Christian Koranteng <korantengchristian@gmail.com>
2023-03-24 05:13:18 +0000

    Clean up and comment code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,9 @@
 
 
 def pascal_triangle(n):
+    """Returns a list of lists of integers representing
+    the Pascal's triangle of n"""
+
     if (n == 0 or n < 0):
         return []
     if (n == 1):
