2190df992888e5560ff787cc1dff43aa06ff262a
Christian Koranteng <korantengchristian@gmail.com>
2023-04-26 11:33:02 +0000

    upload test

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,49 +1,40 @@
 #!/usr/bin/python3
-
 """ UTF-8 Validation """
 
 
 def validUTF8(data):
     """
     Determines if a given data set represents a valid UTF-8 encoding.
-
-    Args:
-        data: A list of integers representing a data set.
-
-    Returns:
-        True if data is a valid UTF-8 encoding, else return False.
+    :param data: a list of integers representing the bytes of the data set
+    :return: True if data is a valid UTF-8 encoding, else return False
     """
-    # Number of bytes in the current UTF-8 character
-    num_bytes = 0
+    # Number of bytes remaining in the current UTF-8 character
+    bytes_remaining = 0
 
-    # Masks to check if the most significant bit (MSB) is set or not
-    mask1 = 1 << 7
-    mask2 = 1 << 6
-
-    # Check each byte in the data set
+    # Iterate through each byte in the data set
     for byte in data:
-        # If this is the start of a new UTF-8 character
-        if num_bytes == 0:
-            # Check how many bytes this UTF-8 character contains
-            mask = 1 << 7
-            while mask & byte:
-                num_bytes += 1
-                mask >>= 1
-
-            # If this is not a valid UTF-8 character, return False
-            if num_bytes == 0:
-                continue
-
-            # If this is a one-byte UTF-8 character, no need to check further
-            if num_bytes == 1:
-                continue
-        else:
-            # If this is not a continuation byte, return False
-            if not (byte & mask1 and not (byte & mask2)):
+        # Check if this byte is the start of a new UTF-8 character
+        if bytes_remaining == 0:
+            # Determine the number of bytes in this UTF-8 character
+            if byte >> 7 == 0b0:
+                # Character is 1 byte long
+                bytes_remaining = 0
+            elif byte >> 5 == 0b110:
+                # Character is 2 bytes long
+                bytes_remaining = 1
+            elif byte >> 4 == 0b1110:
+                # Character is 3 bytes long
+                bytes_remaining = 2
+            elif byte >> 3 == 0b11110:
+                # Character is 4 bytes long
+                bytes_remaining = 3
+            else:
+                # Invalid UTF-8 character start byte
                 return False
-
-        # We've read a valid continuation byte
-        num_bytes -= 1
-
-    # If there are no more continuation bytes expected, the data is valid UTF-8
-    return num_bytes == 0
+        else:
+            # This byte should be a continuation byte
+            if byte >> 6 != 0b10:
+                # Invalid UTF-8 continuation byte
+                return False
+            bytes_remaining -= 1
+    return bytes_remaining == 0
