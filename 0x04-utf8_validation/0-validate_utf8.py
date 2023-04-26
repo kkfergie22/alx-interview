5240c7765b813cabb7faac4d2cf33133bc513ddc
Christian Koranteng <korantengchristian@gmail.com>
2023-04-26 12:05:21 +0000

    Upload new 0 bit

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -16,34 +16,28 @@ def validUTF8(data):
     # Number of bytes in the current UTF-8 character
     num_bytes = 0
 
-    # Masks to check if the most significant bit (MSB) is set or not
-    mask1 = 1 << 7
-    mask2 = 1 << 6
-
     # Check each byte in the data set
     for byte in data:
         # If this is the start of a new UTF-8 character
         if num_bytes == 0:
             # Check how many bytes this UTF-8 character contains
-            mask = 1 << 7
-            while mask & byte:
-                num_bytes += 1
-                mask >>= 1
-
-            # If this is not a valid UTF-8 character, return False
-            if num_bytes == 0:
-                continue
-
-            # If this is a one-byte UTF-8 character, no need to check further
-            if num_bytes == 1:
-                continue
+            if byte >> 7 == 0b0:
+                num_bytes = 1
+            elif byte >> 5 == 0b110:
+                num_bytes = 2
+            elif byte >> 4 == 0b1110:
+                num_bytes = 3
+            elif byte >> 3 == 0b11110:
+                num_bytes = 4
+            else:
+                return False
         else:
             # If this is not a continuation byte, return False
-            if not (byte & mask1 and byte & mask2):
+            if byte >> 6 != 0b10:
                 return False
 
-        # We've read a valid continuation byte
-        num_bytes -= 1
+            # We've read a valid continuation byte
+            num_bytes -= 1
 
     # If there are no more continuation bytes expected, the data is valid UTF-8
     return num_bytes == 0
