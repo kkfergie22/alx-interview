11f6b4f04f05d13c2bb4389ff30e76ba8f943e4d
Christian Koranteng <korantengchristian@gmail.com>
2023-05-08 13:37:31 +0000

    Upload new files

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,34 +4,22 @@ const request = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-request(apiUrl, (error, response, body) => {
+request(apiUrl, async function (error, response, body) {
   if (error) {
-    console.error(error);
-    return;
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (let i = 0; i < characters.length; i++) {
+      const character = await new Promise((resolve, reject) => {
+        request(characters[i], function (error, response, body) {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body).name);
+          }
+        });
+      });
+      console.log(character);
+    }
   }
-
-  if (response.statusCode !== 200) {
-    console.error(`Request failed with status code ${response.statusCode}`);
-    return;
-  }
-
-  const data = JSON.parse(body);
-  const characters = data.characters;
-
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-
-      if (response.statusCode !== 200) {
-        console.error(`Request failed with status code ${response.statusCode}`);
-        return;
-      }
-
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    });
-  });
 });
