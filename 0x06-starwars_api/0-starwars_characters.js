2e1060fa2d9f3ad8db193dc23787e126d4281d63
Christian Koranteng <korantengchristian@gmail.com>
2023-05-08 13:34:39 +0000

    Upload new file

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,33 +1,37 @@
 #!/usr/bin/node
-const request = require('request'); // import request package
-const movieId = process.argv[2]; // get movie ID from command line argument
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`; // construct API URL
 
-request(apiUrl, { json: true }, (err, res, data) => {
-  if (err) {
-    console.error(err);
-  } else if (res.statusCode !== 200) {
-    console.error(`Error: ${res.statusCode} - ${res.statusMessage}`);
-  } else {
-    // get the characters list in the correct order
-    const characters = data.characters;
-    const characterUrls = characters.map((characterUrl) => {
-      const characterId = characterUrl.match(/\/([0-9]*)\/$/)[1];
-      return { url: characterUrl, id: parseInt(characterId) };
-    });
-    characterUrls.sort((a, b) => a.id - b.id);
+const request = require('request');
+const movieId = process.argv[2];
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-    // fetch each character URL in the correct order and print the character name
-    characterUrls.forEach((characterUrl) => {
-      request(characterUrl.url, { json: true }, (err, res, characterData) => {
-        if (err) {
-          console.error(err);
-        } else if (res.statusCode !== 200) {
-          console.error(`Error: ${res.statusCode} - ${res.statusMessage}`);
-        } else {
-          console.log(characterData.name);
-        }
-      });
-    });
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
   }
+
+  if (response.statusCode !== 200) {
+    console.error(`Request failed with status code ${response.statusCode}`);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  const characters = data.characters;
+
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      if (response.statusCode !== 200) {
+        console.error(`Request failed with status code ${response.statusCode}`);
+        return;
+      }
+
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  });
 });
