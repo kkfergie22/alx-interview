9005f39864b94836607054a78b72e4c06206960a
Christian Koranteng <korantengchristian@gmail.com>
2023-05-08 13:23:10 +0000

    Upload new files

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,28 @@
 #!/usr/bin/node
-const fetch = require('node-fetch'); // import node-fetch package
+const request = require('request'); // import request package
 const movieId = process.argv[2]; // get movie ID from command line argument
 const apiUrl = `https://swapi.dev/api/films/${movieId}/`; // construct API URL
 
-fetch(apiUrl) // make API request
-  .then((response) => response.json()) // parse response as JSON
-  .then((data) => {
+request(apiUrl, { json: true }, (err, res, data) => {
+  if (err) {
+    console.error(err);
+  } else if (res.statusCode !== 200) {
+    console.error(`Error: ${res.statusCode} - ${res.statusMessage}`);
+  } else {
     // handle response data
     const characters = data.characters; // extract character URLs from response
     characters.forEach((characterUrl) => {
       // loop through character URLs
-      fetch(characterUrl) // make API request for each character
-        .then((response) => response.json()) // parse response as JSON
-        .then((characterData) => {
+      request(characterUrl, { json: true }, (err, res, characterData) => {
+        if (err) {
+          console.error(err);
+        } else if (res.statusCode !== 200) {
+          console.error(`Error: ${res.statusCode} - ${res.statusMessage}`);
+        } else {
           // handle response data for each character
           console.log(characterData.name); // print character name
-        })
-        .catch((error) => console.error(error)); // handle errors
+        }
+      });
     });
-  })
-  .catch((error) => console.error(error)); // handle errors
+  }
+});
