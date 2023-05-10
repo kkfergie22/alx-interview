208c5f9ac19e7a37a5d04c1d9c0a89db9ada6bfa
Christian Koranteng <korantengchristian@gmail.com>
2023-05-10 07:43:44 +0000

    Try reverse method

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -10,16 +10,13 @@ def rotate_2d_matrix(matrix):
     :param matrix: A list of n lists, each containing n integers.
     :return: None.
     """
-    # transpose matrix
-    transpose_matrix = [
-        [matrix[j][i] for j in range(len(matrix))]
-        for i in range(len(matrix[0]))
-    ]
-    # swap the columns of the transposed matrix with it's rows
-    for i in range(len(transpose_matrix)):
-        transpose_matrix[i][0], transpose_matrix[i][-1] = (
-            transpose_matrix[i][-1],
-            transpose_matrix[i][0],
-        )
-    # replace the original matrix with the transposed matrix
-    matrix[:] = transpose_matrix
+    # reverse the matrix so that bottom row comes to the top
+    matrix.reverse()
+
+    # transpose the reversed matrix so that columns become rows
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+            matrix[j][i], matrix[i][j] = (
+                matrix[i][j],
+                matrix[j][i],
+            )
