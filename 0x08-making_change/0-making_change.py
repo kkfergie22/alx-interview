b28c62643a39560c9182777943365fd93a7522ea
Christian Koranteng <korantengchristian@gmail.com>
2023-05-17 15:13:18 +0000

    Update to bottom up dp

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -20,12 +20,11 @@ def makeChange(coins, total):
 
     if total <= 0:
         return 0
-    for i in range(1, total + 1):
-        for coin in coins:
+    for coin in coins:
+        for i in range(1, total + 1):
             if coin <= i:
                 minimum_number_array[i] = min(minimum_number_array[i],
                                               minimum_number_array[i-coin] + 1)
-                print(minimum_number_array[total])
     if minimum_number_array[total] == float('inf'):
         return -1
     else:
