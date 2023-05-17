c91cd447419f836e5194707543e36a49186f91a7
Christian Koranteng <korantengchristian@gmail.com>
2023-05-17 15:19:34 +0000

    Update Error checking

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,12 +14,12 @@ def makeChange(coins, total):
         total (int): The target total for which we want to
         calculate the minimum number of coins.
     """
+    if total <= 0:
+        return 0
 
     minimum_number_array = [float('inf')] * (total + 1)
     minimum_number_array[0] = 0
 
-    if total <= 0:
-        return 0
     for coin in coins:
         for i in range(1, total + 1):
             if coin <= i:
