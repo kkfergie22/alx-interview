b9e0c089ca2b6538545a5b6238734ff616c312b1
Christian Koranteng <korantengchristian@gmail.com>
2023-05-17 16:30:53 +0000

    Reset

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -18,17 +18,14 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    coin.sort(reverse=True)
-
     minimum_number_array = [float('inf')] * (total + 1)
     minimum_number_array[0] = 0
 
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin > i:
-                break
-            minimum_number_array[i] = min(minimum_number_array[i],
-                                          minimum_number_array[i-coin] + 1)
+    for coin in coins:
+        for i in range(1, total + 1):
+            if coin <= i:
+                minimum_number_array[i] = min(minimum_number_array[i],
+                                              minimum_number_array[i-coin] + 1)
     if minimum_number_array[total] == float('inf'):
         return -1
     else:
